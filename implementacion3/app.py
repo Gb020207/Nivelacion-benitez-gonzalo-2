0d81b7f97c8bc5bcfca0b5bc1e65fcd616cbdf9e
Gb020207 <gonezen123@gmail.com>
2026-03-11 14:24:50 -0300

    Se realizo la implementacion 3 con un servidor hecho con python

---
+++ implementacion3/app.py
@@ -0,0 +1,10 @@
+import http.server
+import socketserver
+
+PORT = 5000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Servidor corriendo en http:localhost:5000")
+    httpd.serve_forever()
